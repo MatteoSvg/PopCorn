06f5d7f63be2cd5a5026810bdcc12dcba4d7147a
Matteo Solavagione <matteo.solavagione@edu-its.it>
2025-05-29 18:04:04 +0200

    addproject

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+    username = 'Matteo Solavagione'
+    logo = 'Logo.svg'
+    get imagePath(){
+      return 'assets/'+ this.logo
+    }
+
+    onUserClick(){
+      console.log('you click me')
+    }
+}
